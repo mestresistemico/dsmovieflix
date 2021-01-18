fbfed26d91eafd49d08c20f2fb3a0b0ee5b27de4
Aurélio L. Andrade <aurelio@sistemico.com.br>
2021-01-18 17:24:39 -0300

    Domain entities created

---
+++ backend/src/main/java/com/mestresistemico/dsmovieflix/entities/Movie.java
@@ -0,0 +1,136 @@
+package com.mestresistemico.dsmovieflix.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_movie")
+public class Movie implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String subtitle;
+	private Integer year;
+	private String imgUrl;
+	@Column(columnDefinition="TEXT")
+	private String synosis;
+	
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
+	private Genre genre;
+	
+	@OneToMany(mappedBy = "movie")
+	private List<Review> reviews = new ArrayList<>();
+	
+	public Movie(Long id, String title, String subtitle, Integer year, String imgUrl, String synosis, Genre genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.subtitle = subtitle;
+		this.year = year;
+		this.imgUrl = imgUrl;
+		this.synosis = synosis;
+		this.genre = genre;
+	}
+
+	public Movie () {}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getSynosis() {
+		return synosis;
+	}
+
+	public void setSynosis(String synosis) {
+		this.synosis = synosis;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
